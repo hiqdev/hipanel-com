3d33520b227adb7954a317cb61864fdcb084cb69
tofid <andreyklochok@gmail.com>
2016-12-12 12:37:33 +0200

    Added new items to menu

--- src/menus/MainMenu.php
+++ src/menus/MainMenu.php
@@ -9,8 +9,12 @@ class MainMenu extends \hiqdev\menumanager\Menu
     public function items()
     {
         return [
-            ['label' => Yii::t('hipanel:com', 'About'), 'url' => ['/site/about']],
-            ['label' => Yii::t('hipanel:com', 'Contact'), 'url' => ['/site/contact']],
+            ['label' => Yii::t('hipanel:com', 'Features'), 'url' => '#features'],
+            ['label' => Yii::t('hipanel:com', 'Screenshots'), 'url' => '#screenshots'],
+            ['label' => Yii::t('hipanel:com', 'Pricing'), 'url' => '#pricing'],
+            ['label' => Yii::t('hipanel:com', 'About'), 'url' => '#about'],
+            ['label' => Yii::t('hipanel:com', 'Team'), 'url' => '#team'],
+            ['label' => Yii::t('hipanel:com', 'Contact'), 'url' => '#contact'],
             ['label' => Yii::t('hipanel:com', 'Login'), 'url' => ['/site/login']],
         ];
     }
