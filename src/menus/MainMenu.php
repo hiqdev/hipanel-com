6991773ef8a7b99c121ead00bce97edb6624b908
tofid <andreyklochok@gmail.com>
2016-12-12 12:47:00 +0200

    Added new template for menu items

--- src/menus/MainMenu.php
+++ src/menus/MainMenu.php
@@ -9,12 +9,12 @@ class MainMenu extends \hiqdev\menumanager\Menu
     public function items()
     {
         return [
-            ['label' => Yii::t('hipanel:com', 'Features'), 'url' => '#features'],
-            ['label' => Yii::t('hipanel:com', 'Screenshots'), 'url' => '#screenshots'],
-            ['label' => Yii::t('hipanel:com', 'Pricing'), 'url' => '#pricing'],
-            ['label' => Yii::t('hipanel:com', 'About'), 'url' => '#about'],
-            ['label' => Yii::t('hipanel:com', 'Team'), 'url' => '#team'],
-            ['label' => Yii::t('hipanel:com', 'Contact'), 'url' => '#contact'],
+            ['label' => Yii::t('hipanel:com', 'Features'), 'url' => '#features', 'template' => '<a href="{url}" class="page-scroll">{label}</a>'],
+            ['label' => Yii::t('hipanel:com', 'Screenshots'), 'url' => '#screenshots', 'template' => '<a href="{url}" class="page-scroll">{label}</a>'],
+            ['label' => Yii::t('hipanel:com', 'Pricing'), 'url' => '#pricing', 'template' => '<a href="{url}" class="page-scroll">{label}</a>'],
+            ['label' => Yii::t('hipanel:com', 'About'), 'url' => '#about', 'template' => '<a href="{url}" class="page-scroll">{label}</a>'],
+            ['label' => Yii::t('hipanel:com', 'Team'), 'url' => '#team', 'template' => '<a href="{url}" class="page-scroll">{label}</a>'],
+            ['label' => Yii::t('hipanel:com', 'Contact'), 'url' => '#contact', 'template' => '<a href="{url}" class="page-scroll">{label}</a>'],
             ['label' => Yii::t('hipanel:com', 'Login'), 'url' => ['/site/login']],
         ];
     }
